236e5d8af1f0d92fbcf1e9f8ff4e8ca444f0a9e9
chau2450 <chau2450@mylaurier.ca>
2023-03-05 20:54:15 -0500

    completed 1 hard problem

--- leet_code/sql/medium/department_highest_salary.sql
+++ leet_code/sql/medium/department_highest_salary.sql
@@ -1,4 +1,17 @@
-select department, name, salary
-from Employee
-left join department
-on 
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary = (SELECT MAX(salary) 
+    FROM Employee 
+    WHERE departmentId = e.departmentId)
+
+
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN
+    (SELECT departmentId, MAX(salary)
+     FROM Employee
+     GROUP BY departmentId)
